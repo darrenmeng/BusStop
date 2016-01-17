10acaad56e07c806adc7d25d2bdb78daf23441ca
ChiangMengTao <meng.tao93@gmail.com>
2016-01-17 18:04:06 +0800

    test
    test

---
+++ helloword.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>我的第一個php程式</title>
+</head>
+
+    <body>
+    
+    <?php
+    
+       echo ("hello world! , 大家好");
+       echo "伺服器的時間為".date("Y/m/d H:i:s");
+      
+	?>
+    </body>
+   
+</html>
